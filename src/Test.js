e61151868ececf450e6e0dc7db7767d3d77b548a
GaziRiad <ouhibimustapha36@gmail.com>
2023-09-21 07:35:53 +0100

    Optimizing Context re-renders

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-function SlowComponent() {
+const SlowComponent = React.memo(function SlowComponent() {
   // If this is too slow on your maching, reduce the `length`
   const words = Array.from({ length: 100_000 }, () => "WORD");
   return (
@@ -11,6 +11,17 @@ function SlowComponent() {
         </li>
       ))}
     </ul>
+  );
+});
+
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+      {children}
+    </div>
   );
 }
 
@@ -23,4 +34,12 @@ export default function Test() {
       <SlowComponent />
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <Counter>
+  //       <SlowComponent />
+  //     </Counter>
+  //   </div>
+  // );
 }
